b8f192c71fd846e42900e3bc001b94be40f041a8
Niels Drost <Niels Drost>
2017-06-22 16:05:19 +0200

    report this example process does support storage and status features we need

--- processes/ultimatequestionprocess.py
+++ processes/ultimatequestionprocess.py
@@ -12,7 +12,7 @@ Done by Jorge de Jesus (jorge.mendesdejesus@wur.nl) as suggested by Kor de Jong
 
 """
 
-from pywps.Process.Process import WPSProcess                                
+from pywps.Process.Process import WPSProcess
 class Process(WPSProcess):
      def __init__(self):
          # init process
@@ -20,22 +20,22 @@ class Process(WPSProcess):
                              identifier="ultimatequestionprocess", #the same as the file name
                              version = "2.0",
                              title="Answer to Life, the Universe and Everything",
-                             storeSupported = "false",
-                             statusSupported = "false",
+                             storeSupported = "true",
+                             statusSupported = "true",
                              abstract="Numerical solution that is the answer to Life, Universe and Everything. The process is an improvement to Deep Tought computer (therefore version 2.0) since it no longer takes 7.5 milion years, but only a few seconds to give a response, with an update of status every 10 seconds.",
                              grassLocation =False)
             #No need for inputs since Execute will start the process
          self.Answer=self.addLiteralOutput(identifier = "answer",
                                             title = "The numerical answer to Life, Universe and Everything")
 
-                                           
+
      def execute(self):
          import time
-         
+
          self.status.set("Preparing....", 0)
          for i in xrange(1, 11):
              time.sleep(5)
-             self.status.set("Thinking.....", float(i*10)) 
-         #The final answer    
+             self.status.set("Thinking.....", float(i*10))
+         #The final answer
          self.Answer.setValue("42")
-        
+
