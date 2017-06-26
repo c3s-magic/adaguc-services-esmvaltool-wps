391142f84f18e455944444102d4eb6bdeaa3c30d
Niels Drost <n.drost@esciencecenter.nl>
2017-06-26 22:35:49 +0200

    creating a few namelists for the demo

--- processes/__init__.py
+++ processes/__init__.py
@@ -1,1 +1,2 @@
-__all__ = ["dummyprocess","ultimatequestionprocess", "esmvaltool-time-series-plot", "pretty-picture"]
+#__all__ = ["dummyprocess","ultimatequestionprocess", "esmvaltool-time-series-plot", "pretty-picture"]
+__all__ = ["esmvaltool-time-series-plot", "esmvaltool-perfmetrics", "esmvaltool-portrait"]
